3dec9be1f30fa66b39324d0d15886fe930e7fb32
funadmin <994927909@qq.com>
2021-06-28 20:03:09 +0800

    '修复插件列表，修复权限缓存'

--- public/static/js/require-addons.js
+++ public/static/js/require-addons.js
@@ -1,20 +1,3 @@
 define([], function () {
-    require.config(
-        {
-            shim: {
-                // codemirror
-                'codemirror':{
-                    deps: [
-                        'css!/static/addons/codemirror/plugins/codemirror/lib/codemirror.css',
-                        'css!/static/addons/codemirror/plugins/codemirror/theme/monokai.css',
-                        'css!/static/addons/codemirror/plugins/codemirror/theme/material.css',
-                    ],
-                    exports: 'codemirror',
-                    init: function(codemirror) {
-                        window.CodeMirror = codemirror;
-                    }
-                },
-            },
-        }
-    );
+
 });
