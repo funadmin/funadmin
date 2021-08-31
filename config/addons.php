6e9fcebb44012749bc69070456ee5a32bc01f780
funadmin <994927909@qq.com>
2021-08-31 09:08:09 +0800

    修复

--- config/addons.php
+++ config/addons.php
@@ -5,27 +5,7 @@ return array (
   'hooks' => 
   array (
   ),
-  'route' => 
-  array (
-    0 => 
-    array (
-      'addons' => 'cms',
-      'domain' => 'cms',
-      'rule' => 
-      array (
-        'cms' => 'cms/frontend/index/index',
-        'cms/download/[:id]' => 'cms/frontend/index/download',
-        'cms/diyform/[:diyid]' => 'cms/frontend/index/diyform',
-        'cms/lists/[:cateid]/[:flag]/[:page]' => 'cms/frontend/index/lists',
-        'cms/show/[:cateid]/[:id]' => 'cms/frontend/index/show',
-        'cms/search/[:keys]/[:flag]/[:page]' => 'cms/frontend/index/search',
-        'cms/error/[:message]' => 'cms/frontend/error/err',
-        'cms/notice/[:message]' => 'cms/frontend/error/notice',
-        'cms/login' => 'cms/frontend/member/login',
-        'cms/register' => 'cms/frontend/member/register',
-        'cms/reset' => 'cms/frontend/member/reset',
-      ),
-    ),
+  'route' => array(
   ),
   'service' => 
   array (
