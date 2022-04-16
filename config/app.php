f0bb08acfc16a21875d3f2344743f5a2c916af04
funadmin <994927909@qq.com>
2022-04-16 15:19:34 +0800

    修复form组件

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     // 显示错误信息
     'show_error_msg'   => true,
     //版本
-    'version' => '2.4.20220416',
-
+    'version' => '2.4.2',
+    'version_data' => '20220416',
     'ip_check'=>false,
 ];
