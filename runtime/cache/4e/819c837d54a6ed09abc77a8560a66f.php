9664f8b2749c4aae2a4ecd85e1154d71bbf630b7
funadmin <994927909@qq.com>
2022-05-27 14:47:38 +0800

    Delete 819c837d54a6ed09abc77a8560a66f.php

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:3:{s:10:"AddonsInit";a:2:{i:0;a:2:{i:0;s:20:"\addons\mocms\Plugin";i:1;s:10:"AddonsInit";}i:1;a:2:{i:0;s:24:"\addons\translate\Plugin";i:1;s:10:"AddonsInit";}}s:8:"demoHook";a:1:{i:0;a:2:{i:0;s:20:"\addons\mocms\Plugin";i:1;s:8:"demoHook";}}s:9:"translate";a:1:{i:0;a:2:{i:0;s:24:"\addons\translate\Plugin";i:1;s:9:"translate";}}}
