3f90d575a7ccb49520baa59126a4e96c39ef7db6
funadmin <994927909@qq.com>
2022-06-01 13:09:21 +0000

    update config/addons.php. 修复addons.php
    Signed-off-by: funadmin <994927909@qq.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <BR>
 
-<h3 align="center">为梦想而创作：FunAdmin开发框架系统 系统支持支持PHP8.X</h3>
+<h3 align="center">为梦想而创作：FunAdmin开发框架系统 系统支持PHP8.X</h3>
 
 <h4 align="center">开源不易，您可以点右上角 "Star" 支持一下 谢谢！</h4>
 <p align="center">
--- config/addons.php
+++ config/addons.php
@@ -1,62 +1,18 @@
 <?php
-
-return array (
-  'autoload' => true,
-  'hooks' => 
-  array (
-    'AddonsInit' => 
-    array (
-      0 => 'mocms',
-      1 => 'translate',
-    ),
-    'DemoHook' => 
-    array (
-      0 => 'mocms',
-    ),
-    'Translate' => 
-    array (
-      0 => 'translate',
-    ),
-  ),
-  'route' => 
-  array (
-    0 => 
-    array (
-      'addons' => 'blog',
-      'domain' => 'blog',
-      'rule' => 
-      array (
-        '/' => 'blog/frontend/index/index',
-        'diyform/[:diyid]' => 'blog/frontend/index/diyform',
-        'lists/[:cateid]/[:flag]/[:page]' => 'blog/frontend/index/lists',
-        'show/[:cateid]/[:id]' => 'blog/frontend/index/show',
-        'search/[:keys]/[:flag]/[:page]' => 'blog/frontend/index/search',
-        'error/[:message]' => 'blog/frontend/error/err',
-        'notice/[:message]' => 'blog/frontend/error/notice',
-      ),
-    ),
-    1 => 
-    array (
-      'addons' => 'mocms',
-      'domain' => 'mocms,testzh,testzh,testzhja',
-      'rule' => 
-      array (
-        '/' => 'mocms/frontend/index/index',
-        'download/[:id]' => 'mocms/frontend/index/download',
-        'diyform/[:cateid]/[:diyid]' => 'mocms/frontend/index/diyform',
-        'lists/[:cateid]/[:flag]/[:page]' => 'mocms/frontend/index/lists',
-        'topic/[:cateid]/[:id]/[:page]' => 'mocms/frontend/index/topic',
-        'show/[:cateid]/[:id]' => 'mocms/frontend/index/show',
-        'search/[:keys]/[:flag]/[:page]' => 'mocms/frontend/index/search',
-        'error/[:message]' => 'mocms/frontend/error/err',
-        'notice/[:message]' => 'mocms/frontend/error/notice',
-        'login' => 'mocms/frontend/member/login',
-        'register' => 'mocms/frontend/member/register',
-        'reset' => 'mocms/frontend/member/reset',
-      ),
-    ),
-  ),
-  'service' => 
-  array (
-  ),
-);
+/**
+ * FunAdmin
+ * ============================================================================
+ * 版权所有 2017-2028 FunAdmin，并保留所有权利。
+ * 网站地址: https://www.FunAdmin.com
+ * ----------------------------------------------------------------------------
+ * 采用最新Thinkphp6实现
+ * ============================================================================
+ * Author: yuege
+ * Date: 2019/10/3
+ */
+return [
+    'autoload' => true,
+    'hooks' => [],
+    'route' => [],
+    'service' => [],
+];
