291f865327ee1d851601bbe25d0ec4c803e5f96f
funadmin <994927909@qq.com>
2022-09-04 11:00:49 +0800

    权限

--- app/common/controller/Backend.php
+++ app/common/controller/Backend.php
@@ -85,6 +85,20 @@ class Backend extends BaseController
     protected $selectpageFields = ['*'];
 
     /**
+     * 无需登录的方法,同时也就不需要鉴权了.
+     *
+     * @var array
+     */
+    protected $noNeedLogin = [];
+
+    /**
+     * 无需鉴权的方法,但需要登录.
+     *
+     * @var array
+     */
+    protected $noNeedRight = [];
+
+    /**
      * @param App $app
      */
     public function __construct(App $app)
--- app/common/controller/Frontend.php
+++ app/common/controller/Frontend.php
@@ -105,6 +105,20 @@ class Frontend extends BaseController
      */
     protected $selectpageFields = ['*'];
 
+    /**
+     * 无需登录的方法,同时也就不需要鉴权了.
+     *
+     * @var array
+     */
+    protected $noNeedLogin = [];
+
+    /**
+     * 无需鉴权的方法,但需要登录.
+     *
+     * @var array
+     */
+    protected $noNeedRight = [];
+
     public function __construct(App $app)
     {
         parent::__construct($app);
