eedbe32fa62aafca77728457c4ee90a60d446cc1
funadmin <994927909@qq.com>
2023-06-23 11:20:41 +0800

    feat:优化

--- app/common.php
+++ app/common.php
@@ -319,3 +319,18 @@ if (!function_exists('getTpVersion')) {
         return App::VERSION;
     }
 }
+
+/**
+ * PHP格式化字节大小
+ * @param number $size      字节数
+ * @param string $delimiter 数字和单位分隔符
+ * @return string            格式化后的带单位的大小
+ */
+if (!function_exists('format_bytes')) {
+    function format_bytes($size, $delimiter = '')
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        for ($i = 0; $size >= 1024 && $i < 5; $i++) $size /= 1024;
+        return round($size, 2) . $delimiter . $units[$i];
+    }
+}
