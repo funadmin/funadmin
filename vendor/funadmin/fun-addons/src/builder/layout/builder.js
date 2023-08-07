35876858c64b48cdb866b87fc4340ec116d4e81c
funadmin <994927909@qq.com>
2023-08-07 23:44:09 +0800

    fix:builder

--- vendor/funadmin/fun-addons/src/builder/layout/builder.js
+++ vendor/funadmin/fun-addons/src/builder/layout/builder.js
@@ -1,44 +1,90 @@
-define(['table','form'], function (Table,Form) {
-    Table.init = {
-        table_elem: tableVars.elem,
-        tableId: tableVars.id,
-        requests: tableVars.requests,
-    }
+// +----------------------------------------------------------------------
+// | FunAdmin全栈开发框架 [基于layui开发]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2020-2030 http://www.funadmin.com
+// +----------------------------------------------------------------------
+// | git://github.com/funadmin/funadmin.git 994927909
+// +----------------------------------------------------------------------
+// | Author: yuege <994927909@qq.com> Apache 2.0 License Code
+define(['table', 'form'], function (Table, Form) {
     let Controller = {
         api: {
             bindevent: function () {
                 Form.api.bindEvent($('form'))
             }
         }
-
     };
-    layui.each(tableVars.actions, function (i, v) {
-        switch (v){
-            case 'index':
-                Controller[v] = function () {
-                var table = Table.render(tableVars['index']);
-                Table.api.bindEvent(Table.init.tableId) ;
-                };
-            break
-            case 'add':
-            case 'edit':
-            case 'copy':
-                Controller[v] = function () {
-                    Controller.api.bindevent()
-                };
-                break;
-            case 'recycle':
-                Controller[v] = function () {
-                    Table.render(tableVars['recycle']);
+    switch (ACTION) {
+        case 'index':
+            if (typeof options !== "undefined" && options[ACTION]) {
+                Controller[ACTION] = function () {
+                    Table.init = {
+                        table_elem: options[ACTION]['elem'],
+                        tableId: options[ACTION]['id'],
+                        requests: requests,
+                    };
+                    options[ACTION]['elem'] = '#' + options[ACTION]['elem']
+                    options[ACTION]['init'] = Table.init;
+                    op = options[ACTION];
+                    console.log(op)
+                    var table = Table.render(options[ACTION]);
                     Table.api.bindEvent(Table.init.tableId);
                 };
-                break;
-            default:
-                Controller[v] = function () {
-                    Controller.api.bindevent()
+            }
+            break;
+        case 'add':
+        case 'edit':
+        case 'copy':
+            if (typeof options !== "undefined" && options[ACTION]) {
+                Controller[ACTION] = function () {
+                    Table.init = {
+                        table_elem: options[ACTION]['elem'],
+                        tableId: options[ACTION]['id'],
+                        requests: requests,
+                    };
+                    options[ACTION]['elem'] = '#' + options[ACTION]['elem'];
+                    options[ACTION]['init'] = Table.init;
+                    var table = Table.render(options[ACTION]);
+                    Table.api.bindEvent(options[ACTION]['id']);
                 };
-                break;
-        }
-    });
+            }
+            Controller[ACTION] = function () {
+                Controller.api.bindevent()
+            };
+            break;
+        case 'recycle':
+            if (typeof options !== "undefined" && options[ACTION]) {
+                Controller[ACTION] = function () {
+                    Table.init = {
+                        table_elem: options[ACTION]['elem'],
+                        tableId: options[ACTION]['id'],
+                        requests: requests,
+                    };
+                    options[ACTION]['elem'] = '#' + options[ACTION]['elem'];
+                    options[ACTION]['init'] = Table.init;
+                    var table = Table.render(options[ACTION]);
+                    Table.api.bindEvent(Table.init.tableId);
+                };
+            }
+            break;
+        default:
+            if (typeof options !== "undefined" && options[ACTION]) {
+                Controller[ACTION] = function () {
+                    Table.init = {
+                        table_elem: options[ACTION]['elem'],
+                        tableId: options[ACTION]['id'],
+                        requests: requests,
+                    };
+                    options[ACTION]['elem'] = '#' + options[ACTION]['elem'];
+                    options[ACTION]['init'] = Table.init;
+                    var table = Table.render(options[ACTION]);
+                    Table.api.bindEvent(options[ACTION]['id']);
+                };
+            }
+            Controller[ACTION] = function () {
+                Controller.api.bindevent()
+            };
+            break;
+    }
     return Controller;
 });
