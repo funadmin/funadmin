c7e621e5fad54929ba7e7a22c4942b0de5249d3d
funadmin <994927909@qq.com>
2023-09-16 00:25:11 +0800

    fix:编辑器

--- public/static/js/require-form.js
+++ public/static/js/require-form.js
@@ -389,8 +389,9 @@ define(['upload'], function (Upload) {
                                 }
                                 window['editor-' + id] = tinymce.init({
                                     selector: '#' + id + '[lay-editor]',
-                                    language: data.language ? data.language : 'zh-Hans',
+                                    language: data.language ||  'zh-Hans',
                                     plugins: data.plugins ? data.plugins : 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+                                    editimage_cors_hosts: ['picsum.photos'],
                                     menubar: 'file edit view insert format tools table help',
                                     toolbar: data.toolbar ? data.toobar : 'undo redo  bold italic underline strikethrough  fontfamily fontsize | blocks  alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  insertfile image media preview  template link  | print  anchor codesample save  ltr rtl ',
                                     toolbar_sticky: false,
