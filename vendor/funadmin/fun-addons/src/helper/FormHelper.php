f170f261b8a17d0da86ce8f4bee67b9d58736387
funadmin <994927909@qq.com>
2023-12-31 11:31:03 +0800

    5.0.7

--- vendor/funadmin/fun-addons/src/helper/FormHelper.php
+++ vendor/funadmin/fun-addons/src/helper/FormHelper.php
@@ -1627,7 +1627,7 @@ EOF;
     {
         list($name, $id) = $this->getNameId($name, $options);
         $value = $this->getValue($name, $value);
-        $value =  $value?'data-value="'.$value.'"':'';
+        $value =  $value?'data-value="'.$value.'" value="'.$value.'"':'';
         return <<<EOF
 name="{$name}" {$value} id="{$id}"
 EOF;
