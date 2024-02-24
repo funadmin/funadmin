3fdbd8fd021cd198bef6e26afa7956e54427d210
funadmin <994927909@qq.com>
2024-02-24 19:48:31 +0800

    fix:增加公共函数

--- app/common.php
+++ app/common.php
@@ -348,10 +348,59 @@ if(!function_exists('password')){
     }
 
 }
+
 // Form别名
 if (!class_exists('BuilderTable')) {
     class_alias('app\\builder\\facade\\BuilderTable', 'BuilderTable');
 }
 if (!class_exists('BuilderForm')) {
     class_alias('app\\builder\\facade\\BuilderForm', 'BuilderForm');
+}
+
+if (!function_exists('getSystemTable')) {
+    /**
+     * @param $table
+     * @param $shift
+     * @return array|string[]
+     * 获取系统表格
+     */
+    function getSystemTable($table=[],$shift= [])
+    {
+        $tableList =  [
+            'addon',
+            'admin',
+            'admin_log',
+            'attach',
+            'attach_group',
+            'auth_group',
+            'auth_rule',
+            'blacklist',
+            'builder',
+            'builder_dict',
+            'builder_dict_value',
+            'builder_field',
+            'builder_with',
+            'config',
+            'config_group',
+            'field_type',
+            'field_verify',
+            'languages',
+            'member',
+            'member_account',
+            'member_address',
+            'member_group',
+            'member_level',
+            'member_third',
+            'oauth2_access_token',
+            'oauth2_client',
+            'provinces',
+        ];
+        if(!empty($table)){
+            $tableList =  array_merge($tableList,$table);
+        }
+        if(!empty($shift)){
+            $tableList = array_diff($tableList, $shift);
+        }
+        return $tableList;
+    }
 }
