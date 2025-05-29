b8d50fed748590b0a103143433f35b9f07df218a
岩坨 <317764739@qq.com>
2025-05-29 14:01:54 +0800

    修复button生成代码bug

--- extend/fun/helper/FormHelper.php
+++ extend/fun/helper/FormHelper.php
@@ -1418,7 +1418,7 @@ EOF;
         }
         $str = <<<EOF
         <input type="hidden" name="__token__" value="{$this->token()} ">
-        <div class=" layui-btn-submit layui-btn-center {$show}" />
+        <div class=" layui-btn-submit layui-btn-center {$show}">
             <button type="submit" class="layui-btn layui-btn-normal submit " lay-fitler="submit" lay-submit>{$this->__('Submit')}
             </button>
             {$reset}
