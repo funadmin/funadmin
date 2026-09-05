c39cbeaead06cfa83a66ecb04c01902b6c7def3b
funadmin <994927909@qq.com>
2026-09-05 10:58:08 +0800

    fix(menu):

---
+++ app/backend/model/MemberGroupRelation.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\backend\model;
+
+/**
+ * 会员与会员组关联模型。
+ */
+class MemberGroupRelation extends BackendModel
+{
+    protected $name = 'member_group_relation';
+
+    protected $pk = ['member_id', 'group_id'];
+
+    protected $autoWriteTimestamp = false;
+}
